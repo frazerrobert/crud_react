9a9debccb42cb373a0470be4b42f9946effdd4a3
frazer-robert <frazer.robert@freshworks.com>
2021-03-04 19:27:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import isPresent from './util';
 
 function App() {
+  const [product, setProduct] = useState([]);
+  const [term, setTerm] = useState('')
+
+  const addProduct = (item) => {
+    if (isPresent(item)) {
+      setProduct(product.concat(item));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" name="Enter Product name" value={term} onChange={(e) => setTerm(e.target.value)} />
+      <button onClick={() => addProduct(term)}>
+        Submit
+      </button>
+      <ul>
+        {product.map((item) => <li key={item}>{item}</li>)}
+      </ul>
     </div>
   );
 }
